9bb9e9542caea4413f5126cf79d632b3288535af
Taggartjoey9 <taggartjoey9@gmail.com>
2025-01-14 20:19:50 -0500

    finised onChange logic for search bar

--- react-moviedux/src/components/MoviesGrid.jsx
+++ react-moviedux/src/components/MoviesGrid.jsx
@@ -5,21 +5,34 @@ export default function MoviesGrid() {
 
     const [movies, setMovies] = useState([]);
 
+    const [searchTerm, setSearchTerm] = useState("");
+
     useEffect(() => {
         fetch("movies.json")
         .then(response => response.json())
         .then(data => setMovies(data))
     })
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    const filteredMovies = movies.filter((movie) => 
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
+        <div>
+            <input onChange={handleSearchChange} type="text" placeholder='Search Movies...' className='search-input' value={searchTerm}/>
         <div className='movies-grid'>
             {
-                movies.map(movie => (
+                filteredMovies.map(movie => (
                     <div key={movie.id} className='movie-card'>
                         <MoviesCard movie={movie} key={movie.id}/>
                     </div>
                 ))
             }
+        </div>
         </div>
     )
 }
